9eae4deecb5f633262b2fc4d0e5eee95b603db1d
MutafAkram223 <mutafakram95@gmail.com>
2024-09-15 06:24:18 -0700

    message

---
+++ Donkey.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main ()
+{ 
+cout << " ----------------------------          " << endl;
+cout << "       o      ^___^                    " << endl;
+cout << "          o   (o o)\\___________        " << endl;
+                                      
+cout << "              (___)\\           )\\//\\    " << endl;
+cout << "                  ||--------W  |       " << endl;
+cout << "                  ||          ||       " << endl;
+cout << "                  ||          ||       " << endl;
+}
---
+++ Pistol.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "    +---^--------------,---------,----------,-----------------------^--,  " << endl;
+    cout << "   |     ||||||||||    '          '             |                       O " << endl;
+    cout << "   |     ||||||||||     ----------              |                       | " << endl;
+    cout << "    '+------------------------------------------------------------------  " << endl;
+    cout << "        ,-----------------,----------------,------------------------,     " << endl;
+    cout << "         // XXXXXXXXXXXXX // ' |  \\\\      |                             " << endl;
+    cout << "        // XXXXXXXXXXXXX //    |   \\\\     |                             " << endl;
+    cout << "       // XXXXXXXXXXXXX // '______________)                               " << endl;
+    cout << "      // XXXXXXXXXXXXX //                                                 " << endl;
+    cout << "     // XXXXXXXXXXXXX //                                                  " << endl;
+    cout << "    // XXXXXXXXXXXXX //                                                   " << endl;
+    cout << "   // XXXXXXXXXXXXX //                                                    " << endl;
+    cout << "  (_______________(                                                       " << endl;
+
+}
